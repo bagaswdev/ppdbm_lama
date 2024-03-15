63f24c2b872c1e1386492e4d7a289e9e1d618118
UgetUget10 <mismunir11@gmail.com>
2024-03-15 09:26:26 +0700

    menambah_migration_baru

--- database/migrations/2024_03_15_021733_create_table_data_grup_wa.php
+++ database/migrations/2024_03_15_021733_create_table_data_grup_wa.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_data_status_siswa', function (Blueprint $table) {
-            $table->id('tb_data_status_siswa_id');
+        Schema::create('table_data_grup_wa', function (Blueprint $table) {
+            $table->id('tb_data_grup_wa_id');
             $table->bigInteger('tb_data_siswa_id');
-            $table->bigInteger('tb_data_jalur_id');
-            $table->string('tb_data_status_siswa_status');
+            $table->string('tb_data_grup_wa_file');
+            $table->string('tb_data_grup_wa_status');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_data_status_siswa');
+        Schema::dropIfExists('table_data_grup_wa');
     }
 };
